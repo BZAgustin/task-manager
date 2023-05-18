9f89bd3cfa1479b318cc3f7bb0bde4a849412f42
BZAgustin <abustoszanelli@gmail.com>
2023-05-18 17:45:53 -0300

    Add taskFactory

--- src/display.js
+++ src/display.js
@@ -19,7 +19,7 @@ const DOM = () => {
 
   // Parent Nodes
   const projectListParent = document.getElementById('project-list');
-  const taskListParent = document.querySelector('task-list');
+  const taskListParent = document.querySelector('.task-list');
 
   // Methods
   function getTitle() {
@@ -42,9 +42,44 @@ const DOM = () => {
     projectListParent.appendChild(project);
   }
 
-  return { btnInbox, btnToday, btnWeek, btnAddProject, 
-           btnNewTask, btnLow, btnMid, btnHigh, btnAddTask, 
+  return { btnInbox, btnToday, btnWeek, btnAddProject, btnNewTask, 
+           btnLow, btnMid, btnHigh, btnAddTask, projectListParent, taskListParent, 
            getTitle, getDescription, getDate, taskListAdd, projectListAdd }
-}
+};
 
-export default DOM;
+const taskFactory = (name, dueDate, taskId) => {
+    const task = document.createElement('div');
+    task.classList.add('task');
+
+    const leftSection = document.createElement('div');
+    const rightSection = document.createElement('div');
+    leftSection.classList.add('left-section');
+    rightSection.classList.add('right-section');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'task-checkbox';
+    const label = document.createElement('label');
+    label.id = `task-${taskId}`;
+    label.innerHTML = name;
+
+    leftSection.appendChild(checkbox);
+    leftSection.appendChild(label);
+
+    const date = document.createElement('input');
+    date.type = 'date';
+    date.value = dueDate;
+
+    const edit = document.createElement('img');
+    edit.alt = 'X';
+
+    rightSection.appendChild(date);
+    rightSection.appendChild(edit);
+
+    task.appendChild(leftSection);
+    task.appendChild(rightSection);
+
+    return task;
+};
+
+export { DOM, taskFactory };
