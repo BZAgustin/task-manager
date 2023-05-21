cb9a696156e77d5c55dc076e11927a0a5151d9ad
BZAgustin <abustoszanelli@gmail.com>
2023-05-21 10:52:42 -0300

    Remove testing lines

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-/* eslint-disable max-classes-per-file */
 import loadApp from './eventController';
 
 loadApp();
-
-// const defaultProject = new Project('Default Project');
-
-// const display = DOM();
-
-// const taskOne = new Task('Do Homework', 'just do your homework man', format(new Date('1994-12-01T03:00:00'), 'dd/MM/yyyy'), '', false);
-// Task.myTasks.push(taskOne);
-
-// const taskTwo = new Task('Shower', 'just shower man', format(new Date('1994-11-05T12:00:00'), 'dd/MM/yyyy'), '', false);
-// Task.myTasks.push(taskTwo);
-
-// const projectOne = new Project('Project 1');
-
-// projectOne.addTask(taskOne);
-// projectOne.addTask(taskTwo);
-
-// Project.myProjects.push(projectOne);
-
-// display.showTasks(Task.myTasks);
-// display.showProjects(Project.myProjects);
