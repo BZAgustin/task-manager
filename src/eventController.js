9c16e498f4f55185ffe3e834b506d41f25f7e86e
BZAgustin <abustoszanelli@gmail.com>
2023-05-21 10:51:31 -0300

    - Add getAllTasks function - Add 'isToday' and 'isThisWeek' - Add listeners for Inbox, Today and This Week buttons

--- src/eventController.js
+++ src/eventController.js
@@ -1,10 +1,44 @@
-import { format, isToday, isThisWeek } from 'date-fns';
+/* eslint-disable no-restricted-syntax */
+
 import DOM from './display';
 import Task from './task';
 import Project from './project';
 
 const display = DOM();
+
 const defaultProject = new Project('Default Project');
+
+const projectManager = {
+  projects: [],
+  activeProject: null
+}
+
+projectManager.activeProject = defaultProject;
+
+function getAllTasks() {
+  const tasks = [];
+
+  tasks.push(defaultProject.myTasks);
+
+  for(const project of projectManager.projects) {
+    tasks.push(project.myTasks);
+  }
+
+  return tasks.flat();
+}
+
+function isToday(date) {
+  const currentDate = new Date().getDate();
+  return date.getDate() === currentDate;
+}
+
+function isThisWeek(date) {
+  const currentDate = new Date();
+  const weekStart = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - currentDate.getDay());
+  const weekEnd = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + (6 - currentDate.getDay()));
+
+  return date >= weekStart && date <= weekEnd;
+}
 
 function plugListeners() {
   display.btnAddTask.addEventListener('click', (e) => {
@@ -12,7 +46,7 @@ function plugListeners() {
     if(display.getTaskName() !== '') {
       const name = display.getTaskName();
       const description = display.getDescription();
-      const date = display.getDate();
+      const date = new Date(`${display.getDate()}`);
       const task = new Task(name, description, date, '', false);
       defaultProject.myTasks.push(task);
       display.showTasks(defaultProject.myTasks);
@@ -22,10 +56,23 @@ function plugListeners() {
   display.btnConfirmProject.addEventListener('click', () => {
     if(display.getProjectName() !== '') {
       const name = display.getProjectName();
-      const project = new Project(name);
-      Project.myProjects.push(project);
-      display.showProjects(Project.myProjects);
+      projectManager.projects.push(new Project(name));
+      display.showProjects(projectManager.projects);
     }
+  });
+
+  display.btnInbox.addEventListener('click', () => {
+    display.showTasks(defaultProject.myTasks);
+  });
+
+  display.btnToday.addEventListener('click', () => {
+    const todayTasks = getAllTasks().filter(task => isToday(task.taskDueDate));
+    display.showTasks(todayTasks);
+  });
+
+  display.btnWeek.addEventListener('click', () => {
+    const weekTasks = getAllTasks().filter(task => isThisWeek(task.taskDueDate));
+    display.showTasks(weekTasks);
   });
 }
 
